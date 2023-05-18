863fe0425f8d57eca7b63f62c954e97d2e780c84
kodanne COULIBALY <kodanne.bakeli@gmail.com>
2023-05-18 23:27:59 +0000

    gestion de temps promessse galerie-photo

--- app.js
+++ app.js
@@ -9,81 +9,81 @@
 
 //text en console
 
-console.log('Texte en console');
+// console.log('Texte en console');
 
 
 //Declaration +affectation
 
-let prenom = "kitty"; //String
+// let prenom = "kitty"; //String
 
-let age = 21; //Number
+// let age = 21; //Number
 
-let enligne = true; //Boolean
+// let enligne = true; //Boolean
 
-let choix = null; //Null
-
-
-console.log(typeof prenom);
-console.log(typeof age);
-console.log(typeof enligne);
-console.log(typeof choix);
-
-//concatenation
-
-console.log(prenom + " " + age + " ans");
-console.log(`Je m'appelle ${prenom}, j'ai ${age} ans`);
-
-//Echapper un caracteres antislash
-console.log("Il fait beau aujourd\'hui");
+// let choix = null; //Null
 
 
-//Declaration et affectation d'une constante
-const VILLE = " Hotel";
+// console.log(typeof prenom);
+// console.log(typeof age);
+// console.log(typeof enligne);
+// console.log(typeof choix);
+
+// //concatenation
+
+// console.log(prenom + " " + age + " ans");
+// console.log(`Je m'appelle ${prenom}, j'ai ${age} ans`);
+
+// //Echapper un caracteres antislash
+// console.log("Il fait beau aujourd\'hui");
 
 
-//Array
-let tab = ["pommes", 30, true, ['vert', 'jaune'], prenom];
+// //Declaration et affectation d'une constante
+// const VILLE = " Hotel";
 
-//Grandeur du tableau
-console.log(tab.length);
-//Index 1
-console.log(tab[1]);
-//Mot vert
-console.log(tab[3][0]);
 
-//Object
-let personnes = {
-    prenom: "kitty",
-    age: 21,
-    enligne: {
-        rue: "12 rue Grand yoff",
-        ville: "rouen"
-    }
-}
-console.log(personnes.prenom)
-console.log(personnes["age"])
-console.log(personnes.enligne.rue)
+// //Array
+// let tab = ["pommes", 30, true, ['vert', 'jaune'], prenom];
 
-let a = 6;
-let b = 2;
+// //Grandeur du tableau
+// console.log(tab.length);
+// //Index 1
+// console.log(tab[1]);
+// //Mot vert
+// console.log(tab[3][0]);
 
-//Addition
-console.log(a + b);
-//Soustraction
-console.log(a - b);
-//Multiplication
-console.log(a * b);
-//Division
-console.log(a / b);
-//Module
-console.log(a % b);
+// //Object
+// let personnes = {
+//     prenom: "kitty",
+//     age: 21,
+//     enligne: {
+//         rue: "12 rue Grand yoff",
+//         ville: "rouen"
+//     }
+// }
+// console.log(personnes.prenom)
+// console.log(personnes["age"])
+// console.log(personnes.enligne.rue)
 
-//Incrementation
-a += 1 //a++ équivaut à: a = a + 1 
-console.log(a);
-//Decrementation
-b -= 1 //b-- équivaut à: b = b - 1 
-console.log(b);
+// let a = 6;
+// let b = 2;
+
+// //Addition
+// console.log(a + b);
+// //Soustraction
+// console.log(a - b);
+// //Multiplication
+// console.log(a * b);
+// //Division
+// console.log(a / b);
+// //Module
+// console.log(a % b);
+
+// //Incrementation
+// a += 1 //a++ équivaut à: a = a + 1 
+// console.log(a);
+// //Decrementation
+// b -= 1 //b-- équivaut à: b = b - 1 
+// console.log(b);
 
 //condition IF
 // let ages = prompt("Saisir votre age");
@@ -146,44 +146,44 @@ console.log(b);
 
 
  //boucles
- for (let i =0; i < 3; i++){
-     console.log("boucle: " + i)
- };
+//  for (let i =0; i < 3; i++){
+//      console.log("boucle: " + i)
+//  };
 
  // parcourir un tableau
-let tabl = ["orange", "mangue", "ananas"]
-console.log("---For---")
-for( let i = 0; i < tabl.length; i+=1){
-    console.log(tabl[i])
-}
-console.log("---while----")
+// let tabl = ["orange", "mangue", "ananas"]
+// console.log("---For---")
+// for( let i = 0; i < tabl.length; i+=1){
+//     console.log(tabl[i])
+// }
+// console.log("---while----")
 
-let j =0;
-while(j <tabl.length){
-    console.log(tabl[j]);
-    j+= 1;
-};
-console.log("---Do while----")
+// let j =0;
+// while(j <tabl.length){
+//     console.log(tabl[j]);
+//     j+= 1;
+// };
+// console.log("---Do while----")
 
-let k =0;
-do{
-    console.log(tabl[k]);
-    k+= 1;
-}while( k <tabl.length);
+// let k =0;
+// do{
+//     console.log(tabl[k]);
+//     k+= 1;
+// }while( k <tabl.length);
 
-// //FOR IN : itérer sur les clés d'un objet
- let voiture = { marque: "peugeot", moteur:"essence"}
- console.log("---for in----")
- for(let k in voiture){
-     console.log(k)
- }
+// // //FOR IN : itérer sur les clés d'un objet
+//  let voiture = { marque: "peugeot", moteur:"essence"}
+//  console.log("---for in----")
+//  for(let k in voiture){
+//      console.log(k)
+//  }
 
-// FOR OF : itérer sur les valeurs d'un tableau
- let tab2 = ["orange", "blanc", "vert"]
- console.log("---For of---")
- for( let v of tab2){
-     console.log(v)
- }
+// // FOR OF : itérer sur les valeurs d'un tableau
+//  let tab2 = ["orange", "blanc", "vert"]
+//  console.log("---For of---")
+//  for( let v of tab2){
+//      console.log(v)
+//  }
 
 
 
@@ -226,23 +226,23 @@ do{
 
  //correction de la vidéo
 
-  const questions = [
-     {
-         question:
-         "A quel compositeur est consacré le film Amadeus ?\n1. Beethoven\n2. Mozart",
-         reponse: 2
-     },
-     {
-         question:
-         "Quelle la capitale de l'Autriche ?\n1. Vienne\n2. Oslo",
-         reponse: 1
-     },
-     {
-         question:
-         "Quel organe est pertubé par les acouphènes ?\n1. Les Pieds\n2. Les Oreilles",
-         reponse: 2
-     }
-  ]
+//   const questions = [
+//      {
+//          question:
+//          "A quel compositeur est consacré le film Amadeus ?\n1. Beethoven\n2. Mozart",
+//          reponse: 2
+//      },
+//      {
+//          question:
+//          "Quelle la capitale de l'Autriche ?\n1. Vienne\n2. Oslo",
+//          reponse: 1
+//      },
+//      {
+//          question:
+//          "Quel organe est pertubé par les acouphènes ?\n1. Les Pieds\n2. Les Oreilles",
+//          reponse: 2
+//      }
+//   ]
 // //  /***************Debut du jeu*********/
 //   let nbBonneReponse = 0;
 
@@ -262,6 +262,15 @@ do{
 //   }
 
 
-  
+const USERS = [
+    {id: 1, name:"Toto"},
+    {id: 2, name:"Tata"},
+    {id: 2, name:"Tutu"}
+];
+const NEWS = [
+    {id: 1, title:"Actu 1"},
+    {id: 2, title:"Actu 2"},
+    {id: 2, title:"Actu 3"}
+];
 
 
--- app2.js
+++ app2.js
@@ -36,21 +36,170 @@
 //     });
 // }
 
-let titles = document.querySelectorAll("#tabs .tab-titles li");
+// let titles = document.querySelectorAll("#tabs .tab-titles li");
 
-for (let title of titles) {
+// for (let title of titles) {
 
-    title.addEventListener("click", function () {
-        // on recupere le numéro du titre
-        let num = this.getAttribute("data-tabtitle");
+//     title.addEventListener("click", function () {
+//         // on recupere le numéro du titre
+//         let num = this.getAttribute("data-tabtitle");
 
-        //on masque la div precedente
-        document.querySelector("#tabs > div.active").classList.remove("active");
+//         //on masque la div precedente
+//         document.querySelector("#tabs > div.active").classList.remove("active");
 
-        //on reactive la div voulu
-        document.querySelector("#tab" + num).classList.add("active");
+//         //on reactive la div voulu
+//         document.querySelector("#tab" + num).classList.add("active");
 
     
-    });
-}
+//     });
+// }
 
+//apparait toutes les x millisecondes
+// window.setInterval(function(){
+//     console.log("Set Interval");
+// },1000);
+
+
+// //apparait apres x millisecondes
+// window.setTimeout(function(){
+//     console.log("Set Interval");
+// },3000);
+
+// //horloge - date
+// const horloge =function(){
+// let date = new Date();
+
+// let heure = date.getHours();
+
+// let minute = date.getMinutes();
+// let seconde = date.getSeconds();
+
+// if(seconde < 10){
+//     seconde = "0" + seconde
+// }
+// if(minute < 10){
+//     minute = "0" + minute
+// }
+
+
+// let affichage = heure + ":" + minute + ":" + seconde;
+
+// document.querySelector("#horloge").innerText = affichage
+// }
+// setInterval(horloge,1000)
+// const images = document.querySelectorAll("#galerie img");
+// let imgActive = 0;
+// images[imgActive].classList.add("show");
+// //masquer les photo sauf la premiere
+
+// for(let i = 1; i < images.length; i+=1){
+//     images[i].classList.add("hidden")
+// };
+// //variable pour stocker l'interval de temps
+// let interval
+// //clic sur le boutton play
+// document.querySelector("#play").addEventListener("click", function(){
+//      interval = setInterval(next,2000)
+// });
+
+// //clic sur le boutton pause
+// document.querySelector("#pause").addEventListener("click" ,function(){
+//     clearInterval(interval);    //destruction de l'interval de temps
+// });
+// //clic sur le boutton suivant
+// document.querySelector("#next").addEventListener("click", function(){
+//     next();
+// });
+// //clic sur le boutton précédent
+// document.querySelector("#prev").addEventListener("click", function(){
+
+//     prev()
+// });
+// //fonction suivante
+//     const next = function(){
+//         images[imgActive].classList.add("hidden");
+//         images[imgActive].classList.remove("show");
+//         imgActive +=1;
+//         if(imgActive >= images.length){
+//             imgActive = 0
+//         };
+//         images[imgActive].classList.remove("hidden");
+//         setTimeout(()=>{
+//             images[imgActive].classList.add("show");
+//         },100)
+//     }
+    
+// //fonction précédente
+// const prev = function (){
+//     images[imgActive].classList.add("hidden");
+//     images[imgActive].classList.remove("show");
+//     imgActive -=1;
+//     if(imgActive <= 0){
+//         imgActive = images.length - 1;
+//     }
+    
+//     images[imgActive].classList.remove("hidden");
+//     setTimeout(()=>{
+//         images[imgActive].classList.add("show");
+//     },100)
+// };
+
+// //gestion touche du clavier
+// window.addEventListener("keydown", function(e){
+//     if( e.key == "ArrowRight"){
+//         next()
+//     }
+//     if( e.key == "ArrowLeft"){
+//         prev()
+//     }
+    
+// })
+//création de la promesse
+const promiseGetUsers = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        if(typeof USERS !== "undefined"){
+            resolve(USERS)
+        }else{
+            reject("Accés aux users impossible")
+        }
+    },2000)
+})
+//recuperer l'etat de la promesse
+
+promiseGetUsers
+.then(function(u){
+    let liste =`<ul>`
+    for(let user of u){
+        liste += `<li>${user.name}</li>`;
+    }
+    liste += `</ul>`
+    document.querySelector("#users").innerHTML =liste
+
+})
+.catch(function(e){
+    console.log(e);
+})
+//****************NEWS**************/
+//création de la promesse
+const promiseGetNews = new Promise((resolve, reject) =>{
+    setTimeout(function(){
+        if(typeof NEWS !== "undefined"){
+            resolve(NEWS)
+        }else{
+            reject("Accés aux news impossible")
+        }
+    },2000)
+})
+//recuperer l'etat de la promesse
+
+promiseGetNews
+.then((n) =>{
+    let liste =`<ul>`
+    for(let actu of n){
+        liste += `<li>${actu.title}</li>`;
+    }
+    liste += `</ul>`
+    document.querySelector("#news").innerHTML =liste
+
+})
+.catch((e) => console.log(e));
